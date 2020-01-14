716eb14333d3d3f001d4f7a702c3a9402ed377ed
Ana Julia <anajuliabit@gmail.com>
2020-01-14 11:39:18 -0300

    backend

---
+++ api/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect(
+  'mongodb+srv://admin:admin@semanaomninstack9-lcork.mongodb.net/omninstack10?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
